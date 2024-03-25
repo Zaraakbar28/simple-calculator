3f7689845c752214c4238cc978bfbd8fce6a013a
zaraakbar28 <zarakbar26@gmail.com>
2024-03-25 22:26:53 +0500

    tweak the text and added console at the end

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 #! /usr/bin/env node
+
 import inquirer from "inquirer";
  
 const answer = await inquirer.prompt([
-  { message: "Enter firstNumber", type: "number", name: "firstNumber" },
-  { message: "Enter secondNumber", type: "number", name: "secondNumber" },
+  { message: "Enter  your firstNumber", type: "number", name: "firstNumber" },
+  { message: "Enter your secondNumber", type: "number", name: "secondNumber" },
   {
     message: "Select one of the operators to perform action",
-    type: "list",
+    type: "list"
     name: "operator",
     choices: ["Addition" , "Subtraction" ,"Multiplication" ,"Division"],
   }, 
@@ -23,6 +24,8 @@ console.log (answer.firstNumber  * answer.secondNumber);
  else if (answer.operator === "Division") {
   console.log(answer.firstNumber  / answer.secondNumber);
   }
+
+console.log('THE END');
 
 
 
